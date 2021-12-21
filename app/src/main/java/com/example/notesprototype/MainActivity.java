76cda4e5834935c697f9adfe71efd040d9a6dbb6
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-21 12:37:45 -0500

    MainActivity - AlertDialog added

--- app/src/main/java/com/example/notesprototype/MainActivity.java
+++ app/src/main/java/com/example/notesprototype/MainActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
+
+    // variables
+    private SharedPreferences sharedPreferences;
+    private String languageName;
+
+    // widgets
+    private TextView mSelectedLanguageText;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,15 +53,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_info)
-                .setTitle("Select Languages")
-                .setMessage("Which language you want to choose?")
-                .setPositiveButton("English", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        mSelectedLanguageText = findViewById(R.id.tvLanguage);
+        sharedPreferences = this.getSharedPreferences("com.example.notesprototype", MODE_PRIVATE);
 
-                    }
-                })
+        if (!sharedPreferences.getString("language", "").isEmpty()) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_info)
+                    .setTitle("Select Languages")
+                    .setMessage("Which language you want to choose?")
+                    .setPositiveButton("English", (dialog, which) -> {
+                        languageName = "English";
+                        mSelectedLanguageText.setText(languageName);
+                        sharedPreferences.edit().putString("language", languageName).apply();
+                    })
+                    .setNegativeButton("Spanish", (dialog, which) -> {
+                        languageName = "Spanish";
+                        mSelectedLanguageText.setText(languageName);
+                        sharedPreferences.edit().putString("language", languageName).apply();
+                    });
+        }
+
+
     }
 }
